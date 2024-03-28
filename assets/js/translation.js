b59e18796e65413a9367f9b2acd37723bfbf34f7
BitNers <wesleyalvessilva@outlook.com.br>
2024-03-28 18:54:47 -0300

    First commit.

---
+++ assets/js/translation.js
@@ -0,0 +1,10 @@
+const translate = [
+        {
+            "pt-br": {
+                "App":{
+                    "Title": "Wesley Silva - Portfólio",
+                    "Version": "1.0.0"
+                }
+            }
+        }
+];
